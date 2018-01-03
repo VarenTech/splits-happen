ea112e1af75907384875f8ad2736b499a3b7c511
Sean McNamara <smcnam@gmail.com>
2018-01-03 17:56:00 -0500

    Fix gutter game test - fails.

--- src/test/java/me/mcnamara/sean/bowling/tests/GameTest.java
+++ src/test/java/me/mcnamara/sean/bowling/tests/GameTest.java
@@ -1,7 +1,9 @@
 package me.mcnamara.sean.bowling.tests;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import me.mcnamara.sean.bowling.Game;
 
@@ -16,8 +18,10 @@ public class GameTest {
 	
 	@Test
 	public void gutterGame() {
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 20; i++) {
 			g.roll(0);
 		}
+		
+		assertEquals(g.score(), 0);
 	}
 }
