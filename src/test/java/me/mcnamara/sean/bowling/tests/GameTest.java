30f451e0b63fd40b66f63f9597871635f93eca9c
Sean McNamara <smcnam@gmail.com>
2018-01-03 17:46:52 -0500

    Implement gutter game test - fails.

--- src/test/java/me/mcnamara/sean/bowling/tests/GameTest.java
+++ src/test/java/me/mcnamara/sean/bowling/tests/GameTest.java
@@ -1,5 +1,23 @@
 package me.mcnamara.sean.bowling.tests;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import me.mcnamara.sean.bowling.Game;
+
 public class GameTest {
 
+	private Game g;
+	
+	@Before
+	public void setUp() {
+		g = new Game();
+	}
+	
+	@Test
+	public void gutterGame() {
+		for(int i = 0; i < 10; i++) {
+			g.roll(0);
+		}
+	}
 }
