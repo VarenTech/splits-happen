a9c88a38742c32ab526f0eb94ee6bb680bf5f152
Sean McNamara <smcnam@gmail.com>
2018-01-03 18:12:17 -0500

    Make nines test pass.

--- src/main/java/me/mcnamara/sean/bowling/Game.java
+++ src/main/java/me/mcnamara/sean/bowling/Game.java
@@ -9,7 +9,20 @@ public class Game {
 	}
 
 	public int score() {
-		return 0;
+		char[] chars = line.toCharArray();
+		int idx = 0;
+		int accum = 0;
+		
+		for(int frame = 0; frame < 10; frame++) {
+			char curr = chars[idx];
+			int numVal = Character.getNumericValue(curr);
+			
+			if(numVal > 0) {
+				accum += numVal;
+			}
+		}
+		
+		return accum;
 	}
 
 }
