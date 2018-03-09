f98de4dfcdb2a4d1370c30487106e5d28058bfbc
Will <williamparks94@gmail.com>
2018-03-09 09:20:41 -0500

    bug fixes and comments
    added comments, closed resource leak, enforced type validation

--- src/Driver.java
+++ src/Driver.java
@@ -8,7 +8,7 @@ public class Driver {
      * @return a HashMap with appropriate keys->values
      */
     private static HashMap<Character, Integer> setUp() {
-        HashMap<Character, Integer> values = new HashMap();
+      HashMap<Character, Integer> values = new HashMap<>();
         values.put('1',1);
         values.put('2', 2);
         values.put('3', 3);
@@ -29,30 +29,37 @@ public class Driver {
      */
     private static int[] getGameInput() {
         Scanner sc = new Scanner(System.in);
-        //just in case strikes are lower case x
-        String input = sc.nextLine().toUpperCase();
+        try {
+            //just in case strikes are lower case x
+            String input = sc.nextLine().toUpperCase();
 
-        char[] inputArray = input.toCharArray();
-        HashMap values = setUp();
+            char[] inputArray = input.toCharArray();
+            //gets the values of the characters
+            HashMap<Character, Integer> values = setUp();
 
-        int[] gameInput = new int[inputArray.length];
+            //initialize an array of ints that holds the frames of the game
+            int[] gameInput = new int[inputArray.length];
 
-        for (int i=0; i < inputArray.length; i++){
-            if (inputArray[i] == '/'){
-                //replace the / with remainder pins
-                gameInput[i] = (10 - (int) values.get(inputArray[i-1]));
-            } else {
-                //convert all chars to integers
-                gameInput[i] = (int) values.get(inputArray[i]);
+            //loops through the input array and places the values into the gameInput array
+            for (int i = 0; i < inputArray.length; i++) {
+                if (inputArray[i] == '/') {
+                    //replace the '/' with remainder pins
+                    gameInput[i] = (10 - (int) values.get(inputArray[i - 1]));
+                } else {
+                    //convert all chars to integers
+                    gameInput[i] = (int) values.get(inputArray[i]);
+                }
             }
+            return gameInput;
+        } finally {
+            //ensures resource cleanup
+            sc.close();
         }
-        return gameInput;
     }
 
 
     public static void main(String ... args) {
         int[] gameInput = getGameInput();
-
         Game g = new Game(gameInput);
         System.out.println("Final Score: " + g.score());
     }
